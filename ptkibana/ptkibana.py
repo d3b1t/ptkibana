1b36ec3da3bebca078f4fd51800d2c16c91a72c8
d3b1t <vit.stankus@gmail.com>
2025-09-04 15:21:04 +0200

    Fetch initial response. Check for kibana

--- ptkibana/ptkibana.py
+++ ptkibana/ptkibana.py
@@ -17,19 +17,76 @@ along with ptkibana.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import argparse
-import sys; sys.path.append(__file__.rsplit("/", 1)[0])
+import sys;
+from difflib import IS_LINE_JUNK
+
+sys.path.append(__file__.rsplit("/", 1)[0])
 
 from _version import __version__
 from ptlibs import ptjsonlib, ptprinthelper, ptmisclib, ptnethelper
 from ptlibs.ptprinthelper import ptprint
+from ptlibs.http.http_client import HttpClient
+from helpers.helpers import Helpers
+import requests
+from modules._is_kibana import IsKibana
 
 class PtKibana:
     def __init__(self, args):
-        self.ptjsonlib   = ptjsonlib.PtJsonLib()
-        self.args        = args
+        self.ptjsonlib = ptjsonlib.PtJsonLib()
+        self.args = args
+        self.http_client = HttpClient(args=self.args, ptjsonlib=self.ptjsonlib)
+        self.helpers = Helpers(args=self.args, ptjsonlib=self.ptjsonlib, http_client=self.http_client)
+
+
+    def _check_if_target_runs_kibana(self) -> None:
+        """
+        Executes the IS_KIBANA pre-check to determine if the target is running Kibana.
+
+        This method:
+        - Instantiates the `_IS_KIBANA` module
+        - Calls its `run()` method
+        - If the module determines that Kibana is NOT running,
+        it calls `ptjsonlib.end_error()` internally and terminates the program.
+
+        Notes:
+            The `_IS_KIBANA` module is responsible for handling the error state
+            and ending execution if the target does not appear to run Kibana.
+        """
+        IsKibana(
+            args=self.args,
+            ptjsonlib=self.ptjsonlib,
+            helpers=self.helpers,
+            http_client=self.http_client,
+            base_response=self.base_response
+        ).run()
+
+    def _fetch_initial_response(self) -> None:
+        """
+        Sends initial HTTP requests to the requested URL. Follows redirects
+        If a non-200 status code is returned, the script exits early.
+        """
+
+        try:
+            # Send request to user specified page via <args.url>
+            self.base_response = self.http_client.send_request(url=self.args.url, method="GET", headers=self.args.headers, allow_redirects=True)
+
+            """
+            if 300 <= self.base_response.status_code < 400:
+                if not self._check_https():
+                    self.ptjsonlib.end_error(f"Redirect to URL: {self.base_response.headers.get('Location', 'unknown')}", self.args.json)
+            """
+            if self.base_response.status_code != 200:
+                self.ptjsonlib.end_error(f"Webpage returns status code: {self.base_response.status_code}", self.args.json)
+
+        except requests.exceptions.RequestException as error_msg:
+            self.ptjsonlib.end_error(f"Error retrieving initial responses:", details=error_msg, condition=self.args.json)
+
 
     def run(self) -> None:
         """Main method"""
+
+        self._fetch_initial_response()
+        self._check_if_target_runs_kibana()
 
         self.ptjsonlib.set_status("finished")
         ptprint(self.ptjsonlib.get_result_json(), "", self.args.json)
